7c70f641917759ffbc75196e42bd05f54fcfeac1
skartyfungus <elliotknochenhauer@gmail.com>
2025-03-04 16:44:53 +0100

    Create Transaction.cs

---
+++ Models/Transaction.cs
@@ -0,0 +1,8 @@
+public class Transaction
+{
+    public int Id { get; set; }
+    public int FromUserId { get; set; }
+    public int ToUserId { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+}
